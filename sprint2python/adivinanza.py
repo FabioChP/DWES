30446d3a96bdaf86e41c57153f3d8ed27b239e26
Fabio Chamorro <fchamorrop22@fpcoruna.afundacion.org>
2023-10-31 12:51:14 +0100

    Python, Ejercicio 2, mejorado el código de adivinanzas para mostrar 3 distintas y contar puntuación

--- sprint2python/adivinanza.py
+++ sprint2python/adivinanza.py
@@ -1,13 +1,24 @@
 adivinanzas = {
-    'adivinanza1' : 'Tengo agujas y no sé coser, tengo números y no sé leer.',
+    'adivinanza1' :'Tengo agujas y no sé coser, tengo números y no sé leer.',
     'respuestas1':'A.- El Reloj   B.- Un Telefono  C.- La Cuchara',
     'solucion1': 'a',
+    'adivinanza2':'Soy bonito por delante y algo feo por detrás, me transformo a cada instante ya que imito a los demás. ¿Sabes quién soy?',
+    'respuestas2':'A.- Una Caja  B.- Un Espejo C.- Una Toalla',
+    'solucion2':'b',
+    'adivinanza3':'Si tú me quieres comer, me verás marrón y peludo, no me podrás romper porque por fuera soy duro.',
+    'respuestas3':'A.- El Cacahuete  B.- EL Platano C.- El Coco',
+    'solucion3':'c',
 }
+puntuacion = 0
 
-print(adivinanzas['adivinanza1'])
-print(adivinanzas['respuestas1'])
-respuesta = input ("Introduce la respuesta (A, B o C):  ")
-if respuesta.lower() == adivinanzas['solucion1']:
-    print("Respuesta correcta")
-else:
-    print("Respuesta NO correcta")
+for i in range(3):
+    print(adivinanzas['adivinanza'+str(i+1)])
+    print(adivinanzas['respuestas'+str(i+1)])
+    respuesta = input ("Introduce la respuesta (A, B o C):  ")
+    if respuesta.lower() == adivinanzas['solucion'+str(i+1)]:
+        print("Respuesta correcta")
+        puntuacion = puntuacion + 10
+    else:
+        print("Respuesta NO correcta")
+        puntuacion = puntuacion -5
+print("Tú puntuación final es :  " + str(puntuacion))
