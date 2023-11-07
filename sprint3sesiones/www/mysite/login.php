f97dc76b2cd0c7a5a7db2f9659ff0bf6ee31f4f5
Fabio Chamorro <fchamorrop22@fpcoruna.afundacion.org>
2023-11-07 14:08:07 +0100

    Sesiones, ejercicio 3,Añadido el código para evitar SQL Inyections

--- sprint3sesiones/www/mysite/login.php
+++ sprint3sesiones/www/mysite/login.php
@@ -13,8 +13,10 @@
                 if ($email == $row[0]) {$registered = 1;}
             }
             if ($registered == 0) {die('El correo no esta registrado');}
-            $query2 = 'SELECT id,contraseña FROM tUsuarios WHERE email = "'.$email.'"';
-            $result2 = mysqli_query($db, $query2) or die('Query error');
+            $query2 = $db -> prepare('SELECT id,contraseña FROM tUsuarios WHERE email = ?');
+            $query2 -> bind_param("s",$email);
+            $query2 -> execute();
+            $result2 = mysqli_stmt_get_result($query2);
             while ($row2 = mysqli_fetch_array($result2)) {
                 if (password_verify($password,$row2[1])) {
                     session_start();
@@ -24,7 +26,7 @@
                     die('Contraseña incorrecta');
                 }
             }
-            
+            $query2 -> close();
             mysqli_close($db);
         ?>
     </body>
