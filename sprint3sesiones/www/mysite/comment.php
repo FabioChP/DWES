f544b680f88483c908ddfd49bd55117d7dff978b
Fabio Chamorro <fchamorrop22@fpcoruna.afundacion.org>
2023-11-09 11:47:47 +0100

    Sesiones, ejercicio 4, cambiado el codigo para evitar inyecciones de SQL

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,5 +1,5 @@
 <?php
-	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail'); // CONEXIÓN ENTRE PHP y la base de datos.
+    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail'); // CONEXIÓN ENTRE PHP y la base de datos.
 ?>
 <html>
     <body>
@@ -7,18 +7,22 @@
             $id = $_POST['id']; // Guarda la id mandada con el formulario en una variable .
             $comentario = $_POST['new_comment']; // Guarda el comentario mandada con el formulario en otra variable.
             session_start();
-            $user_id = 'NULL';
+            $user_id;
             if (!empty($_SESSION['user_id'])) {
                 $user_id = $_SESSION['user_id'];
             }
             $currentDateTime = new DateTime('now'); // Guarda el dia actual en otra variable.
             $currentDate = $currentDateTime->format('Ymd'); // Esta linea formatea la fecha para que se vea (yyyymmdd - 2023-10-23)
-            $query = "INSERT INTO tComentarios(comentario, usuario_id, juego_id, fecha) VALUES ('".$comentario."',".$user_id.",".$id.",".$currentDate.")"; // Este comando sirve para introducir los datos que tenemos en php dentro de la base de datos, lo guardamos en una variable
-            mysqli_query($db, $query) or die('Error'); // Y le mandamos el comando SQL de la linea anterior.
+
+            $query = $db -> prepare("INSERT INTO tComentarios(comentario, usuario_id, juego_id, fecha) VALUES (?,?,?,?)");
+            $query -> bind_param("sis",$comentario,$user_id,$id,$currentDate);
+            $query -> execute();
+
             echo "<p>Nuevo comentario ";
             echo mysqli_insert_id($db); // MUESTRA QUE NÚMERO DE INSERCIÓN ES
             echo " añadido</p>";
             echo "<a href='/detail.php?id=".$id."'>Volver</a>";
+            $query -> close();
             mysqli_close($db); // Cerramos la conexión entre PHP y la base de datos
         ?>
     </body>
