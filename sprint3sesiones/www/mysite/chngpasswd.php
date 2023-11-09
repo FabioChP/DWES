9774a46fa7cdfe2783aef2a4fbfebe67e89cce28
Fabio Chamorro <fchamorrop22@fpcoruna.afundacion.org>
2023-11-09 19:14:42 +0100

    Sesiones, ejercicio 8, añadida la comprobación de contraseña antigua

--- sprint3sesiones/www/mysite/chngpasswd.php
+++ sprint3sesiones/www/mysite/chngpasswd.php
@@ -1,5 +1,6 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail'); // CONEXIÓN ENTRE PHP y la base de datos.
+    $oldpass = $_POST['old_pass'];
     $pass1 = $_POST['pass1'];
     $pass2 = $_POST['pass2'];
     if (empty($pass1) || empty($pass2)) {die('No puede haber campos vacios');}
@@ -11,11 +12,15 @@
     } else {
         die ('Debes tener la sesión iniciada para poder cambiar la contraseña');
     }
-    $query = $db -> prepare("UPDATE tUsuarios SET contraseña=? where id=?;");
-    $query -> bind_param("si",$new_pass,$user_id);
-    $query -> execute();
+    $query = 'SELECT contraseña FROM tUsuarios WHERE id='.$user_id;
+    $result = mysqli_query($db, $query);
+    $row = mysqli_fetch_array($result);
+    if (!password_verify($oldpass,$row[0])) {die('Contraseña Incorrecta');}
+    $query2 = $db -> prepare("UPDATE tUsuarios SET contraseña=? where id=?;");
+    $query2 -> bind_param("si",$new_pass,$user_id);
+    $query2 -> execute();
     echo "<h1>CONTRASEÑA CAMBIADA</h1>";
     echo "<a href=/main.php>Volver al principio</a>";
-    $query -> close();
+    $query2 -> close();
     mysqli_close($db);
 ?>
