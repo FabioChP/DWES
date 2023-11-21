3783db4854e6b2564e05a9d13f69d0fa385a19e1
Fabio Chamorro <fchamorrop22@fpcoruna.afundacion.org>
2023-11-21 13:16:56 +0100

    Django, ejercicio 1, creado el segundo endpoint

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -18,3 +18,20 @@ def devolver_juegos(request):
         diccionario['genero'] = fila_sql.genero
         respuesta_final.append(diccionario)
     return JsonResponse(respuesta_final, safe=False)
+
+def devolver_juegos_por_id(request, id_solicitado):
+    juego = Tjuegos.objects.get(id=id_solicitado)
+    comentarios = juego.tcomentarios_set.all()
+    lista_comentarios = []
+    for fila_comentario_sql in comentarios:
+        diccionario = {}
+        diccionario['id'] = fila_comentario_sql.id
+        diccionario['comentario'] = fila_comentario_sql.comentario
+        lista_comentarios.append(diccionario)
+    resultado = {
+        'id': juego.id,
+        'nombre': juego.nombre,
+        'genero': juego.genero,
+        'comentarios': lista_comentarios
+    }
+    return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
