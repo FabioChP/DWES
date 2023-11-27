c398611a50c9dac588e253c5a8aa65044df010bc
Fabio Chamorro <fchamorrop22@fpcoruna.afundacion.org>
2023-11-27 12:43:34 +0100

    Django, opcional 3, levantado el error 404 y mejorado el template de index.html

--- tutorial_django/mysite/polls/views.py
+++ tutorial_django/mysite/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404,render
+
 from .models import Question
 
 def index(request):
@@ -10,9 +13,9 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-
 def detail(request, question_id):
-    return HttpResponse("You are looking at the question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response="You are looking at the results of question %s."
